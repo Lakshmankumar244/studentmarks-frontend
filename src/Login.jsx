788cac0ffbcb72af5a39e84047833f09cec4b86e
Lakshmankumar244 <lakshmankumarabk244@gmail.com>
2026-01-15 00:07:06 +0530

    cleaned login component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,46 +1,3 @@
-// import axios from "axios";
-// import { useState } from "react";
-
-// function Login(){
-
-//     const [username, setUsername] = useState("");
-//     const [password, setPassword] = useState("");
-
-//     const handleSubmit = async () => {
-
-//     const formData = new URLSearchParams();
-//     formData.append("username",username);
-//     formData.append("password",password);
-
-//     const URL = "http://localhost:8001"
-
-//     const response =await axios.post(
-//         URL + "/auth/token",
-//         formData,
-//         {
-//             headers: {
-//                 "Content-Type": "application/x-www-form-urlencoded"
-//             }
-//         }
-//     )
-//     console.log(response.data)
-//     // const token = response.data.access_token
-//     // localStorage.setItem("token",token)
-// }
-//     return(
-//         <form onSubmit={handleSubmit}>
-//             <h2>Login</h2>
-//             <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
-//             <input type="text" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-//             <button type="submit">Login</button>
-//         </form>
-//     )
-// }
-
-
-// export default Login
-
-
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -68,8 +25,6 @@ function Login() {
           },
         }
       );
-
-      console.log(response.data);
 
       const token = response.data.access_token;
       localStorage.setItem("token", token);
